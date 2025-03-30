f3ced0f9f1667a312d58a0f11e48b3280c59e07c
rahulkumarhavit@gmail.com <allblogwork24@gmail.com>
2025-03-30 23:37:02 +0530

    pydantic validation added

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+app = FastAPI()
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published:bool = True
+
+@app.get("/")
+async def root():
+    return {"message":"hello world from fastapi app"}
+
+
+@app.post("/createposts")
+async def create_posts(new_post:Post):
+    print(new_post)
+    return {"message":f" name is {new_post.title} created"}
