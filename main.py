065ef104913b3505b83f55d159efee63d4479e52
rahulkumarhavit@gmail.com <allblogwork24@gmail.com>
2025-04-06 23:29:23 +0530

    python api development

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 app = FastAPI()
 
 # creating a model by pydantic
@@ -9,6 +10,7 @@ class Post(BaseModel):
     title:str
     content:str
     published:bool = True
+    rating : Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -18,4 +20,14 @@ async def root():
 @app.post("/createposts")
 async def create_posts(new_post:Post):
     print(new_post)
+    print(new_post.model_dump())
+    print(new_post.dict(),"dictionary method")
     return {"message":f" name is {new_post.title} created"}
+
+@app.get("/posts/latest")
+async def get_latest_post():
+    return {"post_id":1,"title":"latest post"}
+
+@app.get("/post/{id}")
+async def get_post(id:int):
+    return {"post_id":id}
