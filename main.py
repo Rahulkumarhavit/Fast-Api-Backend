cfbdb0433e1baad4adf92390bc1d73f2daf3003d
rahulkumarhavit@gmail.com <allblogwork24@gmail.com>
2025-04-07 23:53:26 +0530

    pydantic data validation , response body

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response , status
+from fastapi import FastAPI, Response , status , HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -17,7 +17,7 @@ async def root():
     return {"message":"hello world from fastapi app"}
 
 
-@app.post("/createposts")
+@app.post("/createposts",status_code=status.HTTP_201_CREATED)
 async def create_posts(new_post:Post):
     print(new_post)
     print(new_post.model_dump())
@@ -32,6 +32,13 @@ async def get_latest_post():
 async def get_post(id:int,response:Response):
 
     if id > 5:
-        response.status_code = 404
-        return {"message":"post not found"}
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message":"post not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post not found {id}")
     return {"post_id":id}
+
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id:int):
+    # delete post logic
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
